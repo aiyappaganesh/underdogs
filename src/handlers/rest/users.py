923d0ce4539dbd481c06ed263dda1d931b967b60
Ashray <ashray.velapanur@gmail.com>
2014-09-20 23:48:18 +0530

    redirecting after data pull

--- src/handlers/rest/users.py
+++ src/handlers/rest/users.py
@@ -15,15 +15,14 @@ networks = {
 
 class MemberDataPullHandler(webapp2.RequestHandler):
 	def get(self):
-		company_id = self.request.get('company_id')
-		company = Company.get_by_id(int(company_id))
+		company_id = int(self.request.get('company_id'))
+		company = Company.get_by_id(company_id)
 		users = User.all().ancestor(company)
 		for user in users:
 			for network, user_data in networks.iteritems():
-				logging.info(network)
-				logging.info(user.key().name())
 				third_party_user = ThirdPartyUser.get_by_key_name(network, parent=user)
-				logging.info(third_party_user)
-				user_data.pull_data(user, third_party_user)
+				if third_party_user:
+					user_data.pull_data(user, third_party_user)
+		self.redirect('/member/list?company_id' + company_id)
 
 app = webapp2.WSGIApplication([	('/api/members/pull_data', MemberDataPullHandler)])
