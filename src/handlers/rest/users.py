3eb9842929ca87644d5f07f2b9b080d3c966e662
Ranju <ranju@b-eagles.com>
2014-09-21 02:58:39 +0530

    Computed an average expertise for the company

--- src/handlers/rest/users.py
+++ src/handlers/rest/users.py
@@ -19,15 +19,25 @@ class MemberDataPullHandler(webapp2.RequestHandler):
         company = Company.get_by_id(company_id)
         users = User.all().ancestor(company)
         influence_total = 0.0
+        expertise_total = {}
         for user in users:
             for network, user_data in networks.iteritems():
                 third_party_user = ThirdPartyUser.get_by_key_name(network, parent=user)
                 if third_party_user:
                     user_data.pull_data(user, third_party_user)
             influence_total += user.influence
+            for expertise in user.expertise:
+            	skill, score = expertise.split(' : ')
+            	if skill not in expertise_total:
+            		expertise_total[skill] = 0.0
+            	expertise_total[skill] += float(score)
         company.influence_avg = (influence_total) / float(users.count())
+        company.expertise_avg = []
+        for skill, score in expertise_total.iteritems():
+        	logging.info('here...')
+        	company.expertise_avg.append(skill + ' : ' + str(score / float(users.count())))
         company.put()
-        logging.info('>>> Put..')
+        
         self.redirect('/member/list?company_id=' + str(company_id))
 
 app = webapp2.WSGIApplication([	('/api/members/pull_data', MemberDataPullHandler)])
--- src/model/company.py
+++ src/model/company.py
@@ -5,3 +5,4 @@ class Company(db.Model):
     email = db.StringProperty()
     details = db.StringProperty(indexed=False)
     influence_avg = db.FloatProperty(indexed=False)
+    expertise_avg = db.StringListProperty(indexed=False)
