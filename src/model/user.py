e69b9ca71fee851b7b34f8e00a0b55bc69e77002
Ashray <ashray.velapanur@gmail.com>
2014-09-21 00:31:06 +0530

    fixed indentation

--- src/model/user.py
+++ src/model/user.py
@@ -1,16 +1,16 @@
 from google.appengine.ext import db
 
 class User(db.Model):
-   	name = db.StringProperty(indexed=False)
-   	influence = db.FloatProperty(indexed=False)
-	expertise = db.StringProperty(indexed=False)
+    name = db.StringProperty(indexed=False)
+    influence = db.FloatProperty(indexed=False)
+    expertise = db.StringProperty(indexed=False)
 
-	def update_score(self, influence_score):
-	    if not self.influence:
-	        self.influence = influence_score
-	    else:
-	        self.influence = (0.5 * self.influence) + (0.5 * influence_score)
-	    self.put()
+    def update_score(self, influence_score):
+        if not self.influence:
+            self.influence = influence_score
+        else:
+            self.influence = (0.5 * self.influence) + (0.5 * influence_score)
+        self.put()
 
     def update_expertise_score(self, expertise_score):
         if not self.expertise:
