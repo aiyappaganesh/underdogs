2a154259df947f740c194d632da8fc75a6efb81c
Ranju <ranju@b-eagles.com>
2014-09-21 03:46:35 +0530

    Sorting startups based on their expertise

--- src/handlers/web/startups.py
+++ src/handlers/web/startups.py
@@ -1,5 +1,9 @@
 import webapp2
 from handlers.web import WebRequestHandler
+from model.skill import Skill
+from model.company import Company
+import operator
+import logging
 
 class StartupsPage(WebRequestHandler):
     def get(self):
@@ -16,13 +20,37 @@ class StartupsRegistrationPage(WebRequestHandler):
 class StartupsCriteriaPage(WebRequestHandler):
     def get(self):
         path = 'startups_search_criteria.html'
-        template_values = {}
+        q = Skill.all()
+        template_values = {'skills' : q.fetch(50)}
         self.write(self.get_rendered_html(path, template_values), 200)
+
+class StartupsSearchResultsPage(WebRequestHandler):
+    def convert_string_list_to_dict(self, str_list):
+        ret_val = {}
+        for param in str_list:
+            skill, score = param.split(' : ')
+            ret_val[skill] = score
+        return ret_val
+
+    def get(self):
+        path = 'startups_search_results.html'
+        skill = self['Skill']
+        q = Company.all()
+        sorted_companies = {}
+        for c in q.fetch(50):
+            expertise_dict = self.convert_string_list_to_dict(c.expertise_avg)
+            score = 0.0
+            if skill in expertise_dict:
+                score = float(expertise_dict[skill])
+            sorted_companies[c] = score
+        sorted_companies = sorted(sorted_companies.iteritems(), key=operator.itemgetter(1), reverse = True)
+        logging.info(sorted_companies)
 
 app = webapp2.WSGIApplication(
     [
         ('/startups/registration', StartupsRegistrationPage),
         ('/startups/search/criteria', StartupsCriteriaPage),
+        ('/startups/search', StartupsSearchResultsPage),
         ('/startups', StartupsPage)
     ]
 )
