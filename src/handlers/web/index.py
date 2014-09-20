79de05f785fdd286a77fee9cced1773dc2efa144
Aiyappa Ganesh <aiyappa@b-eagles.com>
2014-09-20 19:56:50 +0530

    Removing add_member endpoint

--- src/handlers/web/index.py
+++ src/handlers/web/index.py
@@ -14,16 +14,9 @@ class StartupRegistrationPage(WebRequestHandler):
         template_values = {}
         self.write(self.get_rendered_html(path, template_values), 200)
 
-class AddMemberPage(WebRequestHandler):
-    def get(self):
-        path = 'add_member.html'
-        template_values = {'members':[{'name':'Test Name 1','score1':'90','score2':'85','score3':'94'},{'name':'Test Name 2','score1':'84','score2':'95','score3':'74'}]}
-        self.write(self.get_rendered_html(path, template_values), 200)
-
 app = webapp2.WSGIApplication(
     [
         ('/', LandingPage),
-        ('/startup_registration', StartupRegistrationPage),
-        ('/add_member', AddMemberPage)
+        ('/startup_registration', StartupRegistrationPage)
     ]
 )
