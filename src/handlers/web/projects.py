7c0c0376b55a5071fef37f4b018d60e5def830c7
Aiyappa Ganesh <aiyappa@b-eagles.com>
2014-10-06 16:11:03 +0530

    Adding select option for skills in project registration

--- src/handlers/web/projects.py
+++ src/handlers/web/projects.py
@@ -1,4 +1,5 @@
 import webapp2
+from model.skill import Skill
 from handlers.web import WebRequestHandler
 from handlers.web.auth import web_login_required
 
@@ -10,6 +11,15 @@ class ProjectsRegistrationPage(WebRequestHandler):
         template_values['duration_options'] = [{'name':'Less than 3 months','value':'3'},
                                                {'name':'3 to 6 months','value':'6'},
                                                {'name':'More than 6 months','value':'12'}]
+        q = Skill.all()
+        skills = q.fetch(50)
+        skill_options = []
+        for skill in skills:
+            skill_option = {}
+            skill_option['name'] = skill.name
+            skill_option['value'] = skill.name
+            skill_options.append(skill_option)
+        template_values['skills'] = skill_options
         self.write(self.get_rendered_html(path, template_values), 200)
 
 
