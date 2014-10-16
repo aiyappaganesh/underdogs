3b47da0a02ee64f5808b3892995db82f627e2c49
Aiyappa Ganesh <aiyappa@b-eagles.com>
2014-10-17 00:55:42 +0530

    Refactoring member list page to use donut chart template

--- src/handlers/web/add_member.py
+++ src/handlers/web/add_member.py
@@ -64,13 +64,21 @@ class ListMemberPage(WebRequestHandler):
         access_type = self.get_access_type(c, user_id)
         q = CompanyMember.all().ancestor(c)
         users = [{'name': User.get_by_key_name(company_member.user_id).name, 'influence': company_member.influence, 'expertise': company_member.expertise} for company_member in q]
-        template_values = { 'company_id' : company_id,
-                            'name' : c.name,
-                            'influence': c.influence_avg if c.influence_avg else 0.0,
-                            'expertise': c.expertise_avg if c.expertise_avg else [],
-                            'users' : users,
-                            'access_type' : access_type,
-                            'admin_id' : user_id}
+        donuts = 2
+        donuts -= 1
+        donut_size = 80-(5*donuts)
+        score_font_size = 40-(3*donuts)
+        tooltip_font_size = 14-donuts
+        template_values = {'company_id': company_id,
+                           'name': c.name,
+                           'influence': c.influence_avg if c.influence_avg else 0.0,
+                           'expertise': c.expertise_avg if c.expertise_avg else [],
+                           'users': users,
+                           'access_type': access_type,
+                           'admin_id': user_id,
+                           'donut_size': donut_size,
+                           'score_font_size': score_font_size,
+                           'tooltip_font_size': tooltip_font_size}
         self.write(self.get_rendered_html(path, template_values), 200)
 
 class MemberLoginPageHandler(WebRequestHandler):
