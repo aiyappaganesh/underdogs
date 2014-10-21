8a7f0e79a231ec9eea756114f285818dc342a1d4
Aiyappa Ganesh <aiyappa@b-eagles.com>
2014-10-21 12:51:28 +0530

    Moving profile data pull to seperate method and also adding ThirdPartyProfileData

--- src/user_data/linkedin.py
+++ src/user_data/linkedin.py
@@ -9,7 +9,6 @@ from google.appengine.ext import deferred
 from model.skill import Skill
 from model.experience import Experience
 from model.education import Education
-from model.user import User
 from datetime import date
 
 import linkedin_config as linkedin
@@ -21,8 +20,21 @@ def pull_data(member, third_party_user):
     patents = response['patents']['_total'] if 'patents' in response else 0
     publications = response['publications']['_total'] if 'publications' in response else 0
     skills = [skill['skill']['name'] for skill in response['skills']['values']] if 'skills' in response and response['skills']['_total'] > 0 else None
-    user = User.get_by_key_name(member.user_id)
+    influence_raw = (4 * connections) + (4 * recommenders) + (1 * patents) + (1 * publications)
+    influence = math.log(influence_raw)/10.0
+    expertise = {}
+    for skill in skills:
+        key = skill.lower()
+        expertise[key] = '0.5'
+        Skill.get_or_insert(key_name=key, name=key)
 
+    influence_score = influence if influence < 1.0 else 1.0
+    member.update_score(influence_score)
+    member.update_expertise_score(expertise)
+
+
+def pull_profile_data(third_party_profile_data):
+    response = json.loads(urlfetch.fetch(linkedin.PROFILE_URL%('positions,educations', third_party_profile_data.access_token)).content)
     if 'positions' in response and response['positions']['_total'] > 0:
         for position in response['positions']['values']:
             title = position['title'] if 'title' in position else ''
@@ -35,7 +47,7 @@ def pull_data(member, third_party_user):
                 endDate = date(position['endDate']['year'], position['endDate']['month'], 1)
             company = position['company']['name'] if 'company' in position and 'name' in position['company'] else ''
             key_name = str(company)+str(startDate)
-            experience = Experience.get_or_insert(key_name=key_name, parent=user)
+            experience = Experience.get_or_insert(key_name=key_name, parent=third_party_profile_data)
             experience.company = company
             experience.title = title
             experience.summary = summary
@@ -55,23 +67,10 @@ def pull_data(member, third_party_user):
             if 'endDate' in education:
                 end = date(education['endDate']['year'], 1, 1)
             key_name = str(school)+str(start)
-            edu = Education.get_or_insert(key_name=key_name, parent=user)
+            edu = Education.get_or_insert(key_name=key_name, parent=third_party_profile_data)
             edu.school = school
             edu.field = field
             edu.degree = degree
             edu.start = start
             edu.end = end
             edu.put()
-
-    influence_raw = (4 * connections) + (4 * recommenders) + (1 * patents) + (1 * publications)
-    influence = math.log(influence_raw)/10.0
-    expertise = {}
-    for skill in skills:
-        key = skill.lower()
-    	expertise[key] = '0.5'
-        Skill.get_or_insert(key_name=key, name=key)
-    
-    influence_score = influence if influence < 1.0 else 1.0
-    member.update_score(influence_score)
-    member.update_expertise_score(expertise)
-
