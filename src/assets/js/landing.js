6a10d1b840e78650edc4ebeaad48937a158a0ae6
Aiyappa Ganesh <aiyappa@b-eagles.com>
2014-11-07 12:14:18 +0530

    Updates to the new landing page

--- src/assets/js/landing.js
+++ src/assets/js/landing.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var top_of_window = $(window).scrollTop();
         var section2 = $('#section-2').offset().top;
-        if(top_of_window >= section2-83) {
+        if(top_of_window >= section2-50) {
             $('header').removeClass('no-navbar');
             $('#nav-section-2').addClass('blue-background');
             $('#nav-section-2 a').addClass('white-font');
