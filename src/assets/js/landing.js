427ea7570da78aca41f940a6c89a71fca9d51fdb
Aiyappa Ganesh <aiyappa@b-eagles.com>
2014-11-10 16:17:49 +0530

    Showing the new home page navbar quicker

--- src/assets/js/landing.js
+++ src/assets/js/landing.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var top_of_window = $(window).scrollTop();
         var section2 = $('#section-2').offset().top;
-        if(top_of_window >= section2-50) {
+        if(top_of_window >= section2-300) {
             $('#nav-icon a').addClass('pull-icon-right');
             $('.navbar-nav').addClass('pull-navmenu-right');
             $('#nav-section-2').addClass('blue-background');
