4d6e150c6bc8fb2717a4d3d2aeb11909e621390a
Aiyappa Ganesh <aiyappa@b-eagles.com>
2014-11-10 14:01:03 +0530

    Updates to navbar on the new home page

--- src/assets/js/landing.js
+++ src/assets/js/landing.js
@@ -3,11 +3,13 @@ $(document).ready(function(){
         var top_of_window = $(window).scrollTop();
         var section2 = $('#section-2').offset().top;
         if(top_of_window >= section2-50) {
-            $('header').removeClass('no-navbar');
+            $('#nav-icon a').addClass('pull-icon-right');
+            $('.navbar-nav').addClass('pull-navmenu-right');
             $('#nav-section-2').addClass('blue-background');
             $('#nav-section-2 a').addClass('white-font');
         } else {
-            $('header').addClass('no-navbar');
+            $('#nav-icon a').removeClass('pull-icon-right');
+            $('.navbar-nav').removeClass('pull-navmenu-right');
         }
     });
 });
