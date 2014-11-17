89a473b1289a77924749acf18978f95a4bbc9feb
Aiyappa Ganesh <aiyappa@b-eagles.com>
2014-11-17 11:43:41 +0530

    Fixing section height issue for new landing page

--- src/assets/js/landing.js
+++ src/assets/js/landing.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
+    set_page_height();
+});
+
+$(document).ready(function(){
+    $(window).resize(function(){
+        set_page_height();
+    });
+});
+
+$(document).ready(function(){
     $(window).scroll(function(){
         var top_of_window = $(window).scrollTop();
         var section2 = $('#section-2').offset().top;
-        if(top_of_window >= section2-300) {
+        if(top_of_window >= section2-($(window).height()/2)) {
             $('#nav-icon a').addClass('pull-icon-right');
             $('.navbar-nav').addClass('pull-navmenu-right');
             $('#nav-section-2').addClass('blue-background');
@@ -13,3 +23,10 @@ $(document).ready(function(){
         }
     });
 });
+
+function set_page_height() {
+    var window_height = $(window).height();
+    console.log(window_height);
+    $('.landing-section').height(window_height);
+    $('.landing-copy').css('margin-top',window_height/2);
+}
