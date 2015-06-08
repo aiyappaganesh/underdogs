650ea461747672992873a800cbf7aad3a71f3114
Ashray <ashray.velapanur@gmail.com>
2015-06-08 18:18:23 +0530

    live apps number now shows correctly on the startups page

--- src/model/design.py
+++ src/model/design.py
@@ -12,18 +12,19 @@ class Design(db.Model):
         self.shots = int(len(dribbble_response))
         self.likes = 0
         for shot in dribbble_response:
-        	self.likes += int(shot['likes_count'])
-        	self.shot_urls.append(shot['images']['normal'])
+            self.likes += int(shot['likes_count'])
+            self.shot_urls.append(shot['images']['normal'])
         self.followers = followers_count
         self.live_apps = 3
 
     @classmethod
     def aggregate_data_for(self, company):
-    	query = Design.all().ancestor(company)
-    	response = {'live_apps':3,'shots':0,'likes':0,'followers':0,'shot_urls':[]}
-    	for member in query:
-    		response['shots'] += member.shots
-    		response['likes'] += member.likes
-    		response['followers'] += member.followers
-    		response['shot_urls'].extend(member.shot_urls)
-    	return response
+        query = Design.all().ancestor(company)
+        response = {'live_apps':0,'shots':0,'likes':0,'followers':0,'shot_urls':[]}
+        for member in query:
+            response['live_apps'] += member.live_apps
+            response['shots'] += member.shots
+            response['likes'] += member.likes
+            response['followers'] += member.followers
+            response['shot_urls'].extend(member.shot_urls)
+        return response
