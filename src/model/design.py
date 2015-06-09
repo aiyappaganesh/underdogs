40c717db39fb000fda3f064c64c755627b07cc45
Ashray <ashray.velapanur@gmail.com>
2015-06-09 14:38:25 +0530

    pulling tags for shots from dribbble

--- src/model/design.py
+++ src/model/design.py
@@ -7,13 +7,17 @@ class Design(db.Model):
     likes = db.IntegerProperty(indexed=False)
     followers = db.IntegerProperty(indexed=False)
     shot_urls = db.StringListProperty(indexed=False)
+    tags = db.StringListProperty(indexed=False)
 
     def update_score(self, dribbble_response, followers_count):
         self.shots = int(len(dribbble_response))
         self.likes = 0
+        tags = []
         for shot in dribbble_response:
             self.likes += int(shot['likes_count'])
             self.shot_urls.append(shot['images']['normal'])
+            tags.extend(shot['tags'])
+        self.tags = list(set(tags))
         self.followers = followers_count
         self.live_apps = 3
 
