da787b27dd551a3e7b5a570644c5edc09d826d05
Aiyappa Ganesh <aiyappa@b-eagles.com>
2015-06-11 18:42:07 +0530

    Changes for new navbar

--- src/assets/js/scroll_nav.js
+++ src/assets/js/scroll_nav.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
 	$(window).scroll(function(){
+	                var window_height = $(window).height();
                     var top = $('body').scrollTop();
-                    if(top <= 0) {
+                    if(top > 120) {
+                        $('header').addClass('animate');
+                    } else {
+                        $('header').removeClass('animate');
+                    }
+                    if(top < window_height) {
                         $('body').removeClass('scrolled');
                     } else {
                         if(!$('body').hasClass('scrolled')){
