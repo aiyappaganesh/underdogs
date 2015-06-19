c108378474b21f2a97a57af83f72d047b3ede589
Ashray <ashray.velapanur@gmail.com>
2015-06-19 13:06:54 +0530

    updated facebook and angellist config files

--- src/facebook_config.py
+++ src/facebook_config.py
@@ -3,7 +3,7 @@ config = {
 	'client_secret': '84db66a17aee44c9595982c00c691336',
 	'auth_url': 'https://www.facebook.com/dialog/oauth',
 	'token_url': '',
-	'redirect_url': 'http://hirepirates.com',
+	'redirect_url': 'http://www.hirepirates.com',
 	'scope': 'email',
 	'login_auth_dialog': 'https://www.facebook.com/dialog/oauth?client_id=%s&redirect_uri=%s',
 	'accesstoken_url': 'https://graph.facebook.com/oauth/access_token?client_id=%s&redirect_uri=%s&client_secret=%s&code=%s'
