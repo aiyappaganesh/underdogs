041fa72afda4780f46e06014371b0ed849cd1ebc
Ranju <ranju@b-eagles.com>
2015-06-25 19:05:37 +0530

    Made donut text bigger

--- src/model/ui_models/donut.py
+++ src/model/ui_models/donut.py
@@ -6,7 +6,7 @@ class Donut():
 		self.container_pos = (size / 2.0) - (self.container_side / 2.0)
 		self.title = title
 		self.score = score
-		self.font_size = self.container_side * 0.3
+		self.font_size = self.container_side * 0.4
 		self.container_color = container_color
 		self.full_color = full_color
 		self.empty_color = empty_color
