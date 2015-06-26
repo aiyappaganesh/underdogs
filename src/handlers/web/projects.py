250f06ba075554ae4bc9bdfff2ef24dd78e75f57
Ashray <ashray.velapanur@gmail.com>
2015-06-26 17:02:46 +0530

    added joint sprints images

--- src/handlers/web/projects.py
+++ src/handlers/web/projects.py
@@ -168,6 +168,7 @@ class ProjectStudyPage(WebRequestHandler):
                 {'bg_color': '#589a2d', 'copy': 'FIND THE BEST DOCTORS AS EASY AS FINDING THE BEST RESTAURANTS', 'image': '/assets/img/study/Comfort-Doctor-Animation.gif'},
                 {'bg_color': '#b04f4f', 'copy': 'GET DIAGNOSIS AND TREATMENT THROUGH TEXT MESSAGE', 'image': '/assets/img/study/Comfort-Interact-Animation.gif'}
         ]
+        template_values['sprint_images'] = ['/assets/img/study/sprints_img_1.png', '/assets/img/study/sprints_img_2.png']
         self.write(self.get_rendered_html(path, template_values), 200)
 
 app = webapp2.WSGIApplication(
