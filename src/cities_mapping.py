1f8e9fd65a3256e9842686d772a7076712ce46cb
Ashray <ashray.velapanur@gmail.com>
2015-07-01 18:53:57 +0530

    updated startup cities mapping

--- src/cities_mapping.py
+++ src/cities_mapping.py
@@ -1,8 +1,8 @@
 cities_map = {
 	'5192735183077376': 'New York',
-	'5642779036221440': 'Los Angeles',
-	'5675267779461120': 'Chicago',
-	'5706803308396544': 'San Francisco',
-	'5755685136498688': 'Philadelphia',
+	'5642779036221440': 'New York',
+	'5675267779461120': 'New York',
+	'5706803308396544': 'Berkeley',
+	'5755685136498688': 'New York',
     'default': 'New York'
 }
